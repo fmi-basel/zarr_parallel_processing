17a4832ca69c2b9cd97b3f0eed60685e937b3462
bugraoezdemir <bugraa.ozdemir@gmail.com>
2024-11-21 10:18:49 +0100

    fix setuptools

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-@author: bugra
-"""
-
-# def parse_requirements(filename):
-#     with open(filename, encoding='utf-8') as fid:
-#         requires = [line.strip() for line in fid.readlines() if line]
-#     return requires
+import setuptools
 
 def readme():
    with open('README.txt') as f:
        return f.read()
-
-# requirements = parse_requirements('requirements.txt')
 
 setuptools.setup(
     name = 'zarr_parallel_processing',
